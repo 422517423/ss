dbe3f808a5d5dbc2ef5a43720afcadc03d950a9f
Administrator <422517423@qq.com>
2017-12-08 17:14:48 +0800

    miaoshu

--- src/SSS.java
+++ src/SSS.java
@@ -2,5 +2,6 @@ public class SSS {
     public static void main(String[] args) {
         System.out.println(445454);
         System.out.println(445454);
+        System.out.println(445454);
     }
 }
